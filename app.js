8781531fbde25b004f736b23d898e30338858db6
Minh Ngo <ngo.minh0224@gmail.com>
2021-01-14 01:23:22 -0800

    updates to app.js

--- app.js
+++ app.js
@@ -1,5 +1,73 @@
 // import functions and grab DOM elements
+import { getRandomThrow } from './get-random-throw.js';
+import { checkResults } from './check-results.js';
+
+const button = document.querySelector('#guess-button');
+const resetButton = document.getElementById('reset-button');
+const current = document.querySelector('#current');
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+const draws = document.querySelector('#draws');
+const resets = document.querySelector('#resets');
+const total = document.querySelector('#total');
 
 // initialize state
+let winsData = 0;
+let drawsData = 0;
+let lossesData = 0;
+let totalGuesses = 0;
+let resetCount = 0;
 
-// set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    totalGuesses++;
+
+    const checkedRadio = document.querySelector('input:checked');
+    const userGuess = checkedRadio.value;
+
+
+    const computerThrow = getRandomThrow();
+
+    const getResult = checkResults(userGuess, computerThrow);
+
+
+    if (getResult === 'draw') {
+        drawsData++;
+        current.textContent = 'You got a draw';
+    } else if (getResult === 'win') {
+        winsData++;
+        current.textContent = 'YOU WON';
+    } else if (getResult === 'lose') {
+        lossesData++;
+        current.textContent = 'YOU LOST';
+    }
+
+    total.textContent = 'total: ' + totalGuesses;
+    wins.textContent = 'wins: ' + winsData;
+    losses.textContent = 'losses: ' + lossesData;
+    draws.textContent = 'draws: ' + drawsData;
+});
+
+const resetTheGame = () => {
+    const doReset = window.confirm('Would you like to play again?');
+
+    if (doReset === true) {
+
+        resetCount++;
+
+        resets.textContent = 'resets: ' + resetCount++;
+        renderResults();
+    } else {
+        return false;
+    }
+};
+resetButton.addEventListener('click', resetTheGame);
+function renderResults() {
+    let winsData = 0;
+    let drawsData = 0;
+    let lossesData = 0;
+    let totalGuesses = 0;
+    total.textContent = 'total plays: ' + totalGuesses;
+    wins.textContent = 'wins: ' + winsData;
+    losses.textContent = 'losses: ' + lossesData;
+    draws.textContent = 'draws: ' + drawsData;
+}
